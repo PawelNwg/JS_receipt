fb4217f18965409b4744a251d47d3edc760f5f86
PawełNiewęgłowski <pawelnwg@gmail.com>
2021-04-09 22:53:12 +0200

    table generation

--- script.js
+++ script.js
@@ -24,19 +24,22 @@ class Receipt {
     this.receiptList = [];
   }
 
-  addProduct(product) {
-    if (this.receiptList.findIndex((x) => x.id == product.id)) {
+  // addProduct(product) {
+  //   if (this.receiptList.findIndex((x) => x.id == product.id)) {
+  //     this.receiptList.push(product);
+  //   } else throw "Produkt o takim ID już istnieje";
+  // }
+  addProduct = (product) => {
+    if (this.receiptList.findIndex((x) => x.id != product.id) < 0) {
       this.receiptList.push(product);
     } else throw "Produkt o takim ID już istnieje";
-  }
+  };
 }
 
-function log() {
-  console.log("eee");
-}
 const myform = document.getElementById("myform");
 // myform.addEventListener("submit", log);
 let R = new Receipt();
+R.addProduct(new Product("Kebabik", 1, 14));
 
 myform.onsubmit = () => {
   let p = new Product(
@@ -47,3 +50,42 @@ myform.onsubmit = () => {
   console.log(p);
   R.addProduct(p);
 };
+
+printTable = () => {
+  var tableHeaders = ["LP", "NAZWA", "ILOŚĆ", "CENA", "SUMA"];
+
+  var myTableDiv = document.getElementById("table");
+
+  var table = document.createElement("TABLE");
+
+  var tableBody = document.createElement("TBODY");
+  table.appendChild(tableBody);
+
+  for (var i = 0; i < R.receiptList.length + 1; i++) {
+    var tr = document.createElement("TR");
+    tableBody.appendChild(tr);
+
+    for (var j = 0; j < 5; j++) {
+      var td = document.createElement("TD");
+      td.width = "120";
+
+      if (i == 0) {
+        td.appendChild(document.createTextNode(tableHeaders[j]));
+      } else if (j == 0)
+        td.appendChild(document.createTextNode(R.receiptList[i - 1].id));
+      else if (j == 1)
+        td.appendChild(document.createTextNode(R.receiptList[i - 1].name));
+      else if (j == 2)
+        td.appendChild(document.createTextNode(R.receiptList[i - 1].quantity));
+      else if (j == 3)
+        td.appendChild(document.createTextNode(R.receiptList[i - 1].price));
+      else if (j == 4)
+        td.appendChild(document.createTextNode(R.receiptList[i - 1].sum));
+
+      tr.appendChild(td);
+    }
+  }
+  myTableDiv.appendChild(table);
+};
+
+printTable();
